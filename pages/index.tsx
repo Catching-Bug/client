46ceed37e4ee6dfce5dd7cfd25f9ff3dc16c485a
MoveRoad <oio3215@naver.com>
2022-07-29 23:50:24 +0900

    Feature : next-auth kakao login 구현

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,20 @@
 import type { NextPage } from 'next'
 import Map from '../src/components/map/map'
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 const Home: NextPage = () => {
-  return <Map latitude={33.5563} longitude={126.79581}></Map>
+  const { data: session } = useSession()
+
+  return (
+    <>
+      <Map latitude={33.5563} longitude={126.79581}></Map>
+      {session ? (
+        <button onClick={() => signOut()}>로그아웃</button>
+      ) : (
+        <button onClick={() => signIn('kakao')}>로그인</button>
+      )}
+    </>
+  )
 }
 
 export default Home
